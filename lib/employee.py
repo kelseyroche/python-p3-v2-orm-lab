cafbf127c9fe458016af83a1cec3cab1fb164afe
Kelsey Roche <roche.kelsey.24@gmail.com>
2024-12-18 14:10:51 -0500

    complete lab

--- lib/employee.py
+++ lib/employee.py
@@ -135,15 +135,12 @@ class Employee:
     def instance_from_db(cls, row):
         """Return an Employee object having the attribute values from the table row."""
 
-        # Check the dictionary for  existing instance using the row's primary key
         employee = cls.all.get(row[0])
         if employee:
-            # ensure attributes match row values in case local instance was modified
             employee.name = row[1]
             employee.job_title = row[2]
             employee.department_id = row[3]
         else:
-            # not in dictionary, create new instance and add to dictionary
             employee = cls(row[1], row[2], row[3])
             employee.id = row[0]
             cls.all[employee.id] = employee
@@ -187,4 +184,10 @@ class Employee:
 
     def reviews(self):
         """Return list of reviews associated with current employee"""
-        pass
+        from review import Review
+        sql="""
+            SELECT * FROM reviews WHERE employee_id=?
+        """
+        CURSOR.execute(sql, (self.id,),)
+        rows=CURSOR.fetchall()
+        return [Review.instance_from_db(row) for row in rows]
